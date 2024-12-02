603f4195761a099d420121ece6c7c112e79d04a0
Andres Felipe Mejia R <mejia9070@gmail.com>
2024-12-02 14:51:29 -0500

    init Sitemap and changes SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,20 @@ import icon from 'astro-icon';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind(), icon(), react()],
   site: 'https://cerrajeroluis24horas.com',
+  integrations: [tailwind(), icon(), react(),
+  sitemap({
+    i18n: {
+      defaultLocale: 'es',
+      locales: { es: 'es-ES', en: 'en-US', fr: 'fr-CA' },
+    }
+  })
+  ],
 });
